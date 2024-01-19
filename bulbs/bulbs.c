a32383e2e360042770e1a42612193f7fdd859576
ThomasWotsits <123269225+ThomasWotsits@users.noreply.github.com>
2024-01-19 11:55:04 +0000

    Add files via upload

---
+++ bulbs/bulbs.c
@@ -0,0 +1,49 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    // Prompt user for a message
+    string message = get_string("Enter message: ");
+
+    // Convert message to decimals
+    for (int i = 0; i < strlen(message); i++)
+    {
+        int decimal = message[i];
+
+        // Convert decimals into 8 bit binary
+        int binary[] = {0, 0, 0, 0, 0, 0, 0, 0};
+        int j = 0;
+        while (decimal > 0)
+        {
+            binary[j] = decimal % 2;
+            decimal = decimal / 2;
+            j++;
+        }
+        // Print binary using print_bulb function
+        for (int k = BITS_IN_BYTE - 1; k >= 0; k--)
+        {
+            print_bulb(binary[k]);
+        }
+        printf("\n");
+    }
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
